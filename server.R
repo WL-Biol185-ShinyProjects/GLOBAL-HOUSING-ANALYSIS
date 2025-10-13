aca44ecec4f5c5101b26c04dda0d3b228ce11473
Buster Couhig <bcouhig@mail.wlu.edu>
2025-10-13 15:43:14 -0400

    Created and made updates to server.R and ui.R

---
+++ server.R
@@ -0,0 +1,10 @@
+
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+
+house <- read.csv("global_house_purchase_dataset.csv")
+
+function(input, output) {
+  
+}
---
+++ ui.R
@@ -0,0 +1,7 @@
+
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+
+house <- read.csv("global_house_purchase_dataset.csv")
+
